3328ab449a6455d538287df5b563aef60dec5620
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-06-19 12:42:13 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Заменить положительные элементы массива на отрицательные
+
+int [] array = new int [5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+}
+void PrintArray(int [] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
+
+void Change()
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1;
+}
+}
+
+PrintArray(array);
+Console.WriteLine();
+Change();
+PrintArray(array);
